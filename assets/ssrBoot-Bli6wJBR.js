48a64e43bda851d4fea2cdcd2d971040b747d57b
GUANNIANZIH <164139266+GUANNIANZIH@users.noreply.github.com>
2024-08-22 05:33:25 +0000

    Deploying to gh-pages from @ GUANNIANZIH/restaurant@5180e648266be86b2142caf3d6ecbeacb90a02ae 🚀

---
+++ assets/ssrBoot-Bli6wJBR.js
@@ -0,0 +1,1 @@
+import{R as t,a2 as e,c as s,bm as n}from"./index-DwXgdl5W.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
